3b96417791eac07259dc92df485ee9ae6646bbe1
VilimTrakos <vilimtrakostanec@gmail.com>
2023-05-03 17:59:03 +0200

    Added SingleCourseStyle and implemented it

--- src/components/CourseArticle/CourseArticle.jsx
+++ src/components/CourseArticle/CourseArticle.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import "./CourseArticle.scss"
+import { SingleCourse as SingleCourseArticle, SingleCourseFigure, SingleCourseImage, SingleCourseContent } from './CourseArticleStyle'
 
 const CourseArticle = ({ imgSrc, imgAlt, content }) => {
     return (
-        <article className="SingleCourse">
-            <figure className="SingleCourse-Figure">
-                <img src={imgSrc} alt={imgAlt} className="SingleCourse-Image" />
-            </figure>
-            <p className="SingleCourse-Content">
-                {content}
-            </p>
-        </article>
+
+        <SingleCourseArticle>
+            <SingleCourseFigure>
+                <SingleCourseImage src={imgSrc} alt={imgAlt} />
+            </SingleCourseFigure>
+            <SingleCourseContent>{content}</SingleCourseContent>
+        </SingleCourseArticle>
+
+        
     )
 }
 

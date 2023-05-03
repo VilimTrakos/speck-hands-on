0e4d13659a0b7529d7a03222d2b5024224cb1a55
VilimTrakos <vilimtrakostanec@gmail.com>
2023-05-03 10:19:38 +0200

    Added HeaderLink to signin & register

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { Header as HeaderWrapper, HeaderInner, Hamburger, Logo, HeaderLink, HeaderButton, HeaderNav } from "./HeaderStyle"
 
-import {Button} from "../../utils/styles/generalStyles"
+
 
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
@@ -15,15 +15,16 @@ const Header = () => {
 
     return (<HeaderWrapper>
         <HeaderInner>
-        <Link to="/">
+        <Link to={"/"}>
             <Logo/>
         </Link>
         <Hamburger/>
         <HeaderNav>
-            <HeaderLink to="/">Home</HeaderLink>
-            <HeaderLink to="/Courses">Courses</HeaderLink>
-            <HeaderButton> Sign in</HeaderButton>
-            <HeaderButton> Register</HeaderButton>
+            <HeaderLink to={"/"}>Home</HeaderLink>
+            <HeaderLink to={"/Courses"}>Courses</HeaderLink>
+            
+            <HeaderLink to={"/SignIn"}><HeaderButton> Sign in</HeaderButton></HeaderLink>
+            <HeaderLink to={"/Register"}><HeaderButton> Register</HeaderButton></HeaderLink>
         </HeaderNav>
         </HeaderInner>
             </HeaderWrapper>
